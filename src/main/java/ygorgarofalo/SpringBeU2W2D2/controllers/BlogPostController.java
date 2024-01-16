c6a423d4114303bfd6c510a5dd2ee8bb62be9af0
Ygor Garofalo <igorgarofalo@gmail.com>
2024-01-16 15:49:52 +0100

    added crud for blogposts class

---
+++ src/main/java/ygorgarofalo/SpringBeU2W2D2/controllers/BlogPostController.java
@@ -0,0 +1,57 @@
+package ygorgarofalo.SpringBeU2W2D2.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ygorgarofalo.SpringBeU2W2D2.entities.BlogPost;
+import ygorgarofalo.SpringBeU2W2D2.services.BlogPostService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/blogposts")
+public class BlogPostController {
+
+    @Autowired
+    private BlogPostService blogPostService;
+
+
+    // GET su una lista di blogPosts
+    @GetMapping
+    public List<BlogPost> getBlogPosts() {
+        return blogPostService.getAllBlogPosts();
+    }
+
+
+    //POST di un BlogPost
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public BlogPost saveBlogPost(@RequestBody BlogPost body) {
+        return blogPostService.saveBlogPost(body);
+    }
+
+    //GET di un Blog Post tramite id
+
+    @GetMapping("/{id}")
+    public BlogPost findById(@PathVariable int id) {
+
+        return blogPostService.findById(id);
+    }
+
+
+    //PUT di un blog Post
+
+    @PutMapping("/{id}")
+    public BlogPost updatePost(@PathVariable int id, @RequestBody BlogPost body) {
+        return blogPostService.findByIdAndUpdate(id, body);
+    }
+
+
+    //DELETE di un blog Post
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBlogPost(@PathVariable int id) {
+        blogPostService.findByIdAndDelete(id);
+    }
+
+}
--- src/main/java/ygorgarofalo/SpringBeU2W2D2/services/BlogPostService.java
+++ src/main/java/ygorgarofalo/SpringBeU2W2D2/services/BlogPostService.java
@@ -1,6 +1,70 @@
 package ygorgarofalo.SpringBeU2W2D2.services;
 
+import org.springframework.stereotype.Service;
+import ygorgarofalo.SpringBeU2W2D2.entities.BlogPost;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@Service
 public class BlogPostService {
 
-    
+    public List<BlogPost> blogPostList = new ArrayList<>();
+
+
+    public List<BlogPost> getAllBlogPosts() {
+        return this.blogPostList;
+    }
+
+
+    public BlogPost saveBlogPost(BlogPost body) {
+        blogPostList.add(body);
+        return body;
+    }
+
+
+    public BlogPost findById(int id) {
+
+        BlogPost found = null;
+
+        for (BlogPost post : blogPostList) {
+            if (post.getId() == id) {
+                found = post;
+            } else {
+                return found;
+            }
+        }
+        return found;
+    }
+
+
+    public BlogPost findByIdAndUpdate(int id, BlogPost body) {
+        for (BlogPost blog : blogPostList) {
+            if (blog.getId() == id) {
+                blog.setId(id);
+                blog.setCategory(body.getCategory());
+                blog.setTitle(body.getTitle());
+                blog.setContent(body.getContent());
+                blog.setReadingTime(body.getReadingTime());
+                blog.setCoverImg(body.getCoverImg());
+                return blog;
+            }
+        }
+        return null;
+    }
+
+
+    public void findByIdAndDelete(int id) {
+        Iterator<BlogPost> blogPostIterator = this.blogPostList.iterator();
+
+        while (blogPostIterator.hasNext()) {
+            BlogPost actualAuthor = blogPostIterator.next();
+
+            if (actualAuthor.getId() == id) {
+                blogPostIterator.remove();
+            }
+        }
+    }
+
 }
